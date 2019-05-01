30e03ba491103bd0840840b3da63c5182a38996f
Christian Theilemann <christian@solvvy.com>
2019-05-01 00:08:45 -0700

    export types / interfaces from module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { SyncCompositeRedactor, SyncCompositeRedactorOptions } from './SyncCompositeRedactor';
 import { AsyncCompositeRedactor, AsyncCompositeRedactorOptions } from './AsyncCompositeRedactor';
 import { GoogleDLPRedactor, GoogleDLPRedactorOptions } from './custom/GoogleDLPRedactor';
+import {
+  AsyncCustomRedactorConfig,
+  CompositeRedactorOptions,
+  IAsyncRedactor,
+  ISyncRedactor,
+  IRedactor,
+  SimpleRegexpCustomRedactorConfig
+} from './types';
 
 export {
   SyncCompositeRedactor as SyncRedactor,
@@ -8,5 +16,11 @@ export {
   AsyncCompositeRedactor as AsyncRedactor,
   AsyncCompositeRedactorOptions,
   GoogleDLPRedactor,
-  GoogleDLPRedactorOptions
+  GoogleDLPRedactorOptions,
+  AsyncCustomRedactorConfig,
+  CompositeRedactorOptions,
+  IAsyncRedactor,
+  ISyncRedactor,
+  IRedactor,
+  SimpleRegexpCustomRedactorConfig
 };
